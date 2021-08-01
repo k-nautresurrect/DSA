251d8ae621bedae779d9e4854f7f94543c51b4ea
k-nautresurrect <kushignite@gmail.com>
2021-08-01 22:39:37 +0530

    Using system call and other function implemented a clock

--- dsa.cpp
+++ dsa.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include<chrono>
+#include<thread>
 
 using namespace std;
 
+
+//first program Hello World!.
 void Namaste_world(){ cout<<"Namaste World!"; }
 
+//simple clock program to understand system calls.
+void Clock(){
+	int hour,minute,seconds = 0;
+	cin>>hour>>minute;
+	while(true){
+		system("clear");
+		for(hour; hour<24; hour++){
+			for(minute; minute<60; minute++){
+				for(seconds=0; seconds<60; seconds++){
+					system("clear");
+					cout<<hour<<" : "<<minute<<" : "<<seconds<<endl;
+					std::chrono::seconds dur(1);
+					std::this_thread::sleep_for(dur);
+				}
+				seconds = 0;
+			}
+			minute = 0;
+		}
+	}
+}
+
 int main(){
-	Namaste_world();
+	Clock();
+	cout<<endl;
 	return 0;
 }
