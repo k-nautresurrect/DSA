bd8e8dbe555918e44ceaed4a4cc362b972a818e8
k-nautresurrect <kushignite@gmail.com>
2021-08-19 08:55:37 +0530

    Modified for traversing the array

--- C/Data-Structures/Array.c
+++ C/Data-Structures/Array.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<ctype.h>
 
 struct Array{
 	int element;
@@ -7,9 +8,21 @@ struct Array{
 };
 
 void PrintArray(struct Array a){
-	puts("Traversed: ");
-	for(int i = 0; i < a.element; i++){
-		printf("%d ",a.arr[i]);
+	char flag = 'N';
+	printf("[T] for traversing\n[R] for reverse traversing\n: ");
+	//in scanf it is scanning whitespace thats why white space is added .
+	scanf(" %c",&flag);
+	if(tolower(flag)=='r'){
+		puts("\nTransversed reverse : ");
+		for(int i = a.element-1; i >= 0; i--){
+			printf("%d ",a.arr[i]);
+		}
+	}	
+	if(tolower(flag)=='t'){
+		puts("\nTraversed: ");
+		for(int i = 0; i < a.element; i++){
+			printf("%d ",a.arr[i]);
+		}
 	}
 }
 
