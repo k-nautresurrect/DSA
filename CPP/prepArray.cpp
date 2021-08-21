f7540b335fef31b7ab959b3ae3004a3c33b354df
k-nautresurrect <kushignite@gmail.com>
2021-08-21 10:08:43 +0530

    Problem 1 on array done

--- CPP/prepArray.cpp
+++ CPP/prepArray.cpp
@@ -2,20 +2,54 @@
 
 using namespace std;
 
-//finding duplicate no. in an array of N+1 integer(if array is of size of n then all element is in range 1 to n-1)
-void dupnoGreedy(vector<int> &arr){
-	for(int i = 0; i<arr.size(); i++ ){
-		for(int j = i+1; j<arr.size(); j++){
+//finding duplicate no. in an array of N+1 integer(without modifing the array)
+//(if array is of size of n then all element is in range 1 to n-1)
+//only one no. is duplicate.
+int dupnoGreedy(vector<int> &arr){
+	int i,j;
+	for(i = 0; i<arr.size(); i++ ){
+		for(j = i+1; j<arr.size(); j++){
 			if(arr[i] == arr[j]){ 
-				cout<<arr[j];
-				break;
+				return arr[j];
 			}
 		}
 	}
-}
+}// time complexity: O(n^2), space complexity: O(1)
+//warning: control reaches end of non-void function [-Wreturn-type]
+//	this warning means that we have to return something from a non void function
+//  however we have returned but in between the loop not outside the scope of loop.
+
+int dupnoHash(vector<int> &arr){
+	vector<int> hash(arr.size(),0);
+	for(int i = 0; i<arr.size(); i++){
+		hash[arr[i]] += 1;
+		if(hash[i]>1){ return i; }
+	}
+}// time complexity: O(n), space complexity: O(n)
+
+int dupnoCycledtct(vector<int> &arr){
+	int slow = arr[0];
+	int fast = arr[0];
+	do{
+		slow = arr[slow];
+		fast = arr[arr[fast]];
+	} while(slow != fast);
+
+	slow = arr[0];
+	while(slow != fast){
+		slow = arr[slow];
+		fast = arr[fast];
+	}
+
+	return slow;
+
+}// time complexity: O(n), space complexity: O(1)
 
 int main(){
 	vector<int> vec{1,2,4,3,4};
-	dupnoGreedy(vec);
+	int dupno = dupnoGreedy(vec);
+	int hashdupno = dupnoHash(vec);
+	int cycledupno = dupnoCycledtct(vec);
+	cout<<dupno<<" "<<hashdupno<<" "<<cycledupno;
 	return 0;
 }
