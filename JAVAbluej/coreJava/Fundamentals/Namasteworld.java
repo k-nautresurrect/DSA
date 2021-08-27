3105ebe42d7458f9838b784ded2ad55f0a44721c
k-nautresurrect <kushignite@gmail.com>
2021-08-27 23:07:37 +0530

    Namaste World program.

--- JAVAbluej/coreJava/Fundamentals/Namasteworld.java
+++ JAVAbluej/coreJava/Fundamentals/Namasteworld.java
@@ -1,1 +1,9 @@
 package Fundamentals;
+public class Namasteworld{
+    public static void main(String[] args){
+        System.out.println("Namaste World !!");
+        System.out.println("Hey there " + args[0]); 
+        // args[1] will work only when we pass the arguments in the main method.
+        
+    }
+}
