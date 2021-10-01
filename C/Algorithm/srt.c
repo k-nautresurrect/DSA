035534baf5db327ac25399486c9212a85be18387
k-nautresurrect <kushignite@gmail.com>
2021-10-01 09:17:48 +0530

    MinMax number program

--- C/Algorithm/srt.c
+++ C/Algorithm/srt.c
@@ -1,28 +1,28 @@
 #include<stdio.h>
 
-int main(){
+int main() {
 	int n;
-	scanf("%d",&n);
-	int a[n+1];
+	scanf("%d", &n);
+	int a[n + 1];
 
-	for(int i = 1; i <= n; i++){
-		scanf("%d",&a[i]);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
 	}
 
-	for(int j = 2; j <= n; j++){
+	for (int j = 2; j <= n; j++) {
 		int key = a[j];
-		int i = j-1;
+		int i = j - 1;
 
-		while(i > 0 && a[i] > key){
+		while (i > 0 && a[i] > key) {
 			a[i + 1] = a[i];
-			i = i-1;
+			i = i - 1;
 		}
 
-		a[i+1] = key;
+		a[i + 1] = key;
 
 	}
 
-	for(int i = 1; i <= n; i++){
-		printf("%d ",a[i]);
+	for (int i = 1; i <= n; i++) {
+		printf("%d ", a[i]);
 	}
 }
