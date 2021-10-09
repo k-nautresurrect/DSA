f6d5494ab044b7fa579fb5e671971c4b4661f10a
k-nautresurrect <kushignite@gmail.com>
2021-10-09 11:47:15 +0530

    Checking prime number.

--- CPP/strt_prog/prime.cpp
+++ CPP/strt_prog/prime.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 
-int main(){
-	
+int main() {
+	int n, i; cin >> n;
+	for (i = 2; i <= n - 1; i++) {
+		if (n % i == 0) {
+			cout << n << "is not prime. " << "\n";
+			break;
+		}
+	}
+	if (i == n) { cout << n << "is prime" << "\n"; }
 	return 0;
 }
