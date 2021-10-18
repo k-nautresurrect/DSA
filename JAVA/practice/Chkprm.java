2fad7971f4d1c577a98858d6a7baeff3487ccea4
k-nautresurrect <kushignite@gmail.com>
2021-10-18 13:24:22 +0530

    Check prime in sqrt complexity

--- JAVA/practice/Chkprm.java
+++ JAVA/practice/Chkprm.java
@@ -48,5 +48,47 @@ public class Chkprm extends IO {
 					output.println(false);
 				}*/
 		// O(sqrt(n))
+		/*
+			25 - (1,25), (5,5), !(25,1).
+				  ^  ^
+				  x  y
+			if x and  is a factor of num
+			x * y = num  ---> 1 25
+
+			x is a itrator = init 2
+			if x is smaller than y  -> itreator y-1 { x ==> y-1 } x++
+			x * x =< num ---> 1 * 1 < 25, 5 * 5 = 25  ----> (situation)
+			x^2 <= num
+			x <= √num
+
+			--> the factor which can result in other situation than this is already taken
+			--> it is mandatory that x is smaller than y
+
+			1 -> prime or compiste
+			1 is neither prime nor composite
+		*/
+		/*
+			output -> boolean ,true,false
+			chck if there is any other no than i which will divide n
+
+
+			1. n -> input
+			2. loop from 2 to x^2 <= n, updating x++
+			3. if terminated completly then prime.
+			4. else not prime.
+		*/
+		int i; // y -> n
+		for (i = 2; i * i <= n; i++) {
+			output.println("i is " + i);
+			output.println("i^2 is " + i * i);
+			if (n % i == 0) {
+				break;
+			}
+		}
+		if (i * i > n) {
+			output.println(true);
+		} else {
+			output.println(false);
+		}
 	}
 }
