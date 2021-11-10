a3c4fe932f2ad042c010ab57a84637611338094c
k-nautresurrect <kushignite@gmail.com>
2021-11-10 13:56:53 +0530

    Removed directory

--- JAVAbluej/coreJava/Fundamentals/Namasteworld.java
+++
@@ -1,9 +0,0 @@
-package Fundamentals;
-public class Namasteworld{
-    public static void main(String[] args){
-        System.out.println("Namaste World !!");
-        System.out.println("Hey there " + args[0]); 
-        // args[1] will work only when we pass the arguments in the main method.
-        
-    }
-}
