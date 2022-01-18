ecffbd3f4f0a625101c66c78118798d9a8a2b39e
k-nautresurrect <kushignite@gmail.com>
2022-01-18 10:24:17 +0530

    Setup For Chain

---
+++ JAVA/Chain/Day1.java
@@ -0,0 +1,19 @@
+/*
+    Take n as an input, for a value of n = 7 the pattern will be
+
+
+                        1
+                    2 1   1 2
+                3 2 1       1 2 3
+            4 3 2 1           1 2 3 4
+                3 2 1       1 2 3
+                    2 1   1 2
+                        1
+
+    N should always be odd number.
+*/
+public class Day1 extends IO {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ JAVA/Chain/IO.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+import java.io.PrintStream;
+
+public class IO {
+	public static Scanner in = new Scanner(System.in);
+	public static PrintStream out = new PrintStream(System.out);
+}
