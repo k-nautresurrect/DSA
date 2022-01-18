c3d334be49d81a4a355f0e51e65ed7800622fa75
k-nautresurrect <kushignite@gmail.com>
2022-01-18 14:26:12 +0530

    Modified Pointer to a struct

--- CPP/Essentials/pointerToAStruct.cpp
+++ CPP/Essentials/pointerToAStruct.cpp
@@ -9,6 +9,9 @@ struct rectangle {
 };
 int main() {
 	struct rectangle r = {10, 5};
+
+	// this is also valid.
+	rectangle rec = {8, 15};
 	struct rectangle *ptr = &r; // pointer can only be assigned by adress
 
 	r.length = 15;
@@ -17,13 +20,25 @@ int main() {
 	*/
 	//*ptr.length = 20; // wrong, because . has higher precedence than *
 	(*ptr).length = 20;
+	// simple way to access using ->
 	ptr->length = 30;
 
 	cout << r.length << endl;
 
-	// Dynamic allocation
+	// Dynamic allocation in heap
 	struct rectangle *point;
 	// malloc(sizeof(struct rectangle)) // returns void pointer.
-	point = (struct rectangle *)malloc(sizeof(struct rectangle));
+	point = (struct rectangle *)malloc(sizeof(struct rectangle)); // C like
+	point->length = 15;
+	point->width = 30;
+
+	struct rectangle *pointer;
+	pointer = new rectangle;
+	pointer->length = 30;
+	pointer->width = 40;
+
+	cout << pointer->length << " " << pointer->width << endl;
+
+	cout << point->length << " " << point->width << endl;
 	return 0;
 }
