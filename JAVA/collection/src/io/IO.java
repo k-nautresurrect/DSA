de382e734f7171a4ce75bc06ea77b3779171dcff
k-nautresurrect <kushignite@gmail.com>
2022-01-20 18:33:03 +0530

    Removed eclipse Project collection

--- JAVA/collection/src/io/IO.java
+++
@@ -1,9 +0,0 @@
-package io;
-
-import java.io.PrintStream;
-import java.util.Scanner;
-
-public class IO {
-	public static Scanner in = new Scanner(System.in);
-	public static PrintStream out = new PrintStream(System.out);
-}
--- JAVA/collection/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author kaynaut
- *
- */
-module collection {
-}
