8be07c8abea4e15b189624cbfd6d4c93db898027
k-nautresurrect <kushignite@gmail.com>
2022-01-20 09:16:08 +0530

    Setup collection in eclipse

---
+++ JAVA/collection/src/lists/Arraylist.java
@@ -0,0 +1,14 @@
+package lists;
+
+public class Arraylist {
+
+	public Arraylist() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JAVA/collection/src/lists/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * this package contains the working of lists and uses of lists collection in java.
+ * ArrayLists
+ * LinkedLists
+ * Vector
+ */
+/**
+ * @author kaynaut
+ *
+ */
+package lists;
