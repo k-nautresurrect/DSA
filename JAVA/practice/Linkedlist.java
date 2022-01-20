debe7b49a2b8500940e4836f4b7370d4774fd059
k-nautresurrect <kushignite@gmail.com>
2022-01-20 12:35:17 +0530

    Added Linkedlist for practice.

---
+++ JAVA/practice/Linkedlist.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Linkedlist {
+	public static void main(String[] args) {
+		LinkedList ls = new LinkedList();
+
+		ls.add(9);
+		ls.add("hello");
+
+		System.out.println(ls);
+	}
+}
