4d327f502ca35a0870a8db47a821371858f6f392
k-nautresurrect <kushignite@gmail.com>
2022-01-20 12:34:23 +0530

    Modified files in collection

---
+++ JAVA/collection/src/lists/ImplementLinkedList.java
@@ -0,0 +1,11 @@
+package lists;
+
+import io.IO;
+
+public class ImplementLinkedList extends IO{
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- JAVA/collection/src/lists/package-info.java
+++ JAVA/collection/src/lists/package-info.java
@@ -1,11 +1,1 @@
-/**
- * this package contains the working of lists and uses of lists collection in java.
- * ArrayLists
- * LinkedLists
- * Vector
- */
-/**
- * @author kaynaut
- *
- */
 package lists;
