6ed5b5db3cda26cc91c2c1eba920c35e97133c0c
k-nautresurrect <kushignite@gmail.com>
2022-01-29 14:20:47 +0530

    primitive data Types in java

--- JAVA/javaSE/README.md
+++ JAVA/javaSE/README.md
@@ -7,4 +7,4 @@
 	- Single program can be compiled using simply ```javac ProgramName.java```
 	- Package can be compiled using ```javac -d source/path dirname/program.java```
 	- it is recommended to make seprate directory for storing class files
-	- 
+	- to see all the method available for a class use ```javap path.to.class```
--- JAVA/javaSE/intro/Main.java
+++ JAVA/javaSE/intro/Main.java
@@ -7,5 +7,11 @@ public class Main extends IO {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		out.print("this is from io");
+
+		int n =  in.nextInt();
+		out.println(in.nextInt()); // this will return the integer.
+		out.println(in.hasNext()); // just check wether input is there in inputStream
+		out.println(in.hasNextInt()); // takes input and then check on it.
+
 	}
 }
