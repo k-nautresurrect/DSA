49e2cd7722f169fb428299ab1e1fe3b769fd3e43
k-nautresurrect <kushignite@gmail.com>
2022-02-02 10:06:19 +0530

    Added Documentation for io in se.

---
+++ JAVA/javaSE/javaseDocs/io/IO.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class IO here.
+ *
+ * @author (k-nautresurrect)
+ * @version (0.0.1)
+ */
+
+import java.util.Scanner;
+import java.io.PrintStream;
+public class IO
+{
+    public static Scanner in = new Scanner(System.in);
+    public static PrintStream out = new PrintStream(System.out);
+}
